1c74c3577ace2af9a84c3b1c3df1b7a37677fb97
123456 <hoochanlon@outlook.com>
2025-10-07 18:44:59 +0800

    asd

---
+++ themes/next/test/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+require('chai').should();
+
+describe('NexT', () => {
+  require('./helpers');
+  require('./tags');
+  require('./validate');
+});
---
+++ themes/next/test/tags/video.js
@@ -0,0 +1,14 @@
+'use strict';
+
+
+describe('video', () => {
+  const postVideo = require('../../scripts/tags/video');
+
+  it('default', () => {
+    postVideo(['https://example.com/sample.mp4']).should.eql('<video src="https://example.com/sample.mp4" preload="metadata" controlslist="nodownload" controls playsinline poster=""></video>');
+  });
+
+  it('poster', () => {
+    postVideo(['https://example.com/sample.mp4', 'https://example.com/sample.jpg']).should.eql('<video src="https://example.com/sample.mp4" preload="metadata" controlslist="nodownload" controls playsinline poster="https://example.com/sample.jpg"></video>');
+  });
+});
