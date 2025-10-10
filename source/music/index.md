5f87bf51a10b239dc2fcc2cfe6f06e197be105d3
123456 <hoochanlon@outlook.com>
2025-10-11 03:05:37 +0800

    add

---
+++ source/music/index.md
@@ -0,0 +1,10 @@
+---
+title: music
+date: 2025-10-11 02:00:54
+aplayer: true
+comments: false
+type: music
+---
+
+
+{% meting "14405552446" "netease" "playlist" "autoplay" "mutex:true" "listmaxheight:1140px" "preload:auto" "theme:#ad7a86"%}
---
+++ themes/butterfly/source/js/aizhaoyao.js
@@ -0,0 +1,5 @@
+let tianliGPT_postSelector = '\#post \#article-container';
+let _gptTokenEncoded = 'Uy0xMldLSElDV1MzNzNXVUh='; 
+let _gptToken = atob(_gptTokenEncoded); 
+// 黑名单 JSON 地址
+let tianliGPT_blacklist = 'https://raw.githubusercontent.com/hoochanlon/hoochanlon.github.io/refs/heads/master/ban-ai-zhaiyao.json';
