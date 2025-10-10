756caf1ae6faceaf2c7682cb5f86744d67546a50
hoochanlon <hoochanlon@users.noreply.github.com>
2025-10-10 23:24:58 +0000

    deploy: 8a68d34cae9b17510283313bff3c6405ce807dae

--- js/aizhaoyao.js
+++ js/aizhaoyao.js
@@ -1,5 +1,4 @@
+
 let tianliGPT_postSelector = '\#post \#article-container';
-let _gptTokenEncoded = 'Uy0xMldLSElDV1MzNzNXVUh='; 
-let tianliGPT_key = atob(_gptTokenEncoded); 
-// 黑名单 JSON 地址
+let tianliGPT_key = 'S-12WKHICUWC373WUH';
 let tianliGPT_blacklist = 'https://raw.githubusercontent.com/hoochanlon/hoochanlon.github.io/refs/heads/master/ban-ai-zhaiyao.json';
