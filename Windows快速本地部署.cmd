9b0fc65c2ef824d5b94557a7daca97460f873426
123456 <hoochanlon@outlook.com>
2025-10-15 00:41:06 +0800

    1

---
+++ Windows快速本地部署.cmd
@@ -0,0 +1,26 @@
+@echo off
+SETLOCAL EnableDelayedExpansion
+
+:: 设置 Hexo 项目路径
+SET HEXO_PATH=%USERPROFILE%\Documents\GitHub\hoochanlon.github.io
+cd /d !HEXO_PATH!
+
+:: 检查依赖是否完整
+echo 正在检查依赖是否完整...
+cmd /c "npm ls --depth=0 >nul 2>nul"
+SET ERR=!ERRORLEVEL!
+echo npm ls 返回值：!ERR!
+
+IF !ERR! NEQ 0 (
+    echo 依赖不完整，执行 npm install...
+    npm install
+   start cmd /k "cd /d %HEXO_PATH% && hexo clean && hexo g && gulp && hexo s"
+) ELSE (
+    echo 依赖完整，无需安装。
+)
+
+:: 保留窗口并启动 Hexo 本地服务
+echo 正在启动 Hexo 本地服务...
+start cmd /k "cd /d %HEXO_PATH% && hexo clean && hexo g && gulp && hexo s"
+ENDLOCAL
+
