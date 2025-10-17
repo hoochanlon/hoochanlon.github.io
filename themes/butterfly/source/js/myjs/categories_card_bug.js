f6badcca05b4cc56c059253ec122bb6bb358b6c4
123456 <hoochanlon@outlook.com>
2025-10-17 17:07:49 +0800

    1

--- themes/butterfly/source/js/myjs/categories_card_bug.js
+++
@@ -1,8 +0,0 @@
-// 获取当前完整路径
-  const currentPath = window.location.pathname;
-
-  // 如果路径以 /categories/categories/ 开头，就重定向到 /categories/
-  if (currentPath.startsWith('/categories/categories/')) {
-    const newPath = currentPath.replace('/categories/categories/', '/categories/');
-    window.location.replace(newPath);
-  }
